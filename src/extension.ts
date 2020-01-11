7855246ab1deb922e3a45cf58494a4c063ee04da
PilockHulmes <392206976@qq.com>
2020-01-11 18:00:35 +0800

    prototool: create the formatter class

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,13 @@
 import * as vscode from 'vscode';
+import ProtobufDocumentFormatter from './formatter';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-prototool" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+	let disposable = vscode.languages.registerDocumentRangeFormattingEditProvider(
+		{ scheme: 'file', language: 'protobuf3' },
+		new ProtobufDocumentFormatter(),
+	);
 
 	context.subscriptions.push(disposable);
 }
---
+++ src/formatter.ts
@@ -0,0 +1,16 @@
+import * as vscode from 'vscode';
+
+export default class ProtobufDocumentFormatter implements vscode.DocumentRangeFormattingEditProvider {
+    public provideDocumentRangeFormattingEdits(
+        document: vscode.TextDocument, range: vscode.Range,
+        options: vscode.FormattingOptions, token: vscode.CancellationToken):
+        vscode.TextEdit[]
+    {
+        let start = new vscode.Position(1,1);
+        let end = new vscode.Position(1,2);
+        let r = new vscode.Range(start, end);
+        let t = new vscode.TextEdit(r, "empty");
+
+        return [t];
+    }
+}
